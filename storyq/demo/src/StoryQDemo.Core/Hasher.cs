dc7abea76067ac2d84279ffb9e645b38b40cc202
Liam McLennan <liam.mclennan@gmail.com>
2010-11-20 13:04:59 +1000

    updated demo

---
+++ storyq/demo/src/StoryQDemo.Core/Hasher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StoryQDemo.Core
+{
+    public interface IHash
+    {
+        string Hash(string hashInput);
+    }
+
+    public class Hasher : IHash
+    {
+        private MD5 hashAlgorithm = MD5.Create();
+
+        public string Hash(string hashInput)
+        {
+            var hashBytes = hashAlgorithm.ComputeHash(Encoding.Default.GetBytes(hashInput));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sBuilder.Append(hashBytes[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ storyq/demo/src/StoryQDemo.Test/AuthenticationBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NSubstitute;
+using NUnit.Framework;
+using StoryQ;
+
+namespace StoryQDemo.Test
+{
+    [TestFixture]
+    public class AuthenticationBehaviour
+    {
+        private Feature authenticate = new Story("Authenticate")
+            .InOrderTo("access secure resources")
+            .AsA("unauthenticated user")
+            .IWant("to sign in");
+
+        [Test]
+        public void authenticate_successfully()
+        {
+            authenticate.WithScenario("authenticate successfully")
+                .Given(AUserWithUsername, "stevesmith")
+                .And(Password, "elvish")
+                .When(TheUserSignsInWith, "stevesmith", "elvish")
+                .Then(TheyAuthenticateSuccessfully).ExecuteWithReport(MethodBase.GetCurrentMethod());
+        }
+
+        [Test]
+        public void authentication_failure()
+        {
+
+        }
+
+        private void AUserWithUsername(string username)
+        {
+            throw new NotImplementedException();
+            #region Live Coding Sucks
+            // user = new User { UserName = username };
+            #endregion
+        }
+
+        private void Password(string password)
+        {
+            throw new NotImplementedException();
+            #region Live Coding Sucks
+            //user.Password = password;
+            #endregion
+        }
+
+        private void TheUserSignsInWith(string username, string password)
+        {
+            throw new NotImplementedException();
+            #region Live Coding Sucks
+            //var dataService = Substitute.For<IDataService>();
+            //dataService.GetUserByUserName(username).Returns(user);
+            //var authenticationService = new AuthenticationService(dataService);
+            //result = authenticationService.IsValidCredentials(username, password);
+            #endregion
+        }
+
+        private void TheyAuthenticateSuccessfully()
+        {
+            throw new NotImplementedException();
+            #region Live Coding Sucks
+            //Assert.IsTrue(result);
+            #endregion
+        }
+
+    }
+
+    public class User
+    {
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+    }
+}
